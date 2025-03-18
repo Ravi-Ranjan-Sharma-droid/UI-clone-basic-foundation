a5c8602ee894e665ea576e6118b33ef1357e0eb5
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-03-19 01:48:39 +0530

    all element added but need to work on position

--- GYM-Website/js/script.js
+++ GYM-Website/js/script.js
@@ -11,10 +11,12 @@ function toggleVideo() {
     const slider = document.querySelector(".swiper");
     const content = document.querySelector(".content");
     const form = document.querySelector('form')
+    const sci = document.querySelector('.sci')
 
     video.classList.toggle("active");
     play.classList.toggle("active");
     slider.classList.toggle("active");
     content.classList.toggle("active");
     form.classList.toggle("active")
+    sci.classList.toggle("active")
 }

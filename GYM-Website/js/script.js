00e5db7133f5c5707a4fe0bfbd8ac149c8e21e27
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-03-18 19:27:42 +0530

    fix typo in script.js

--- GYM-Website/js/script.js
+++ GYM-Website/js/script.js
@@ -1,19 +1,18 @@
-function toggleNav(){
-    const nav = document.querySelector('.nav')
-    const menu = document.querySelector('.menu')
-    nav.classList.toggle('active')
-    menu.classList.toggle('active')
+function toggleNav() {
+    const nav = document.querySelector(".nav");
+    const menu = document.querySelector(".menu");
+    nav.classList.toggle("active");
+    menu.classList.toggle("active");
 }
 
-function toggelVideo() {
-    const video = document.querySelector('.bg-video');
-    const play = document.querySelector('.play');
-    const slider = document.querySelector('.swipe');
-    const content = document.querySelector('.content');
+function toggleVideo() {
+    const video = document.querySelector(".bg-video");
+    const play = document.querySelector(".play");
+    const slider = document.querySelector(".swiper");
+    const content = document.querySelector(".content");
 
-    video.classList.toggle('active')
-    play.classList.toggle('active')
-    slider.classList.toggle('active')
-    content.classList.toggle('active') 
-    
+    video.classList.toggle("active");
+    play.classList.toggle("active");
+    slider.classList.toggle("active");
+    content.classList.toggle("active");
 }
